b00e06cbf4bd078e28d1a3504f3130a0c90f85a6
Ali Imam Akash <akash1437aliimam@gmail.com>
2025-11-10 23:00:16 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('book heaven server is running')
-})
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yxfp204.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("book heaven server is running");
+});
 
 app.listen(port, () => {
-  console.log(`book heaven server listening on port ${port}`)
-})
+  console.log(`book heaven server listening on port ${port}`);
+});
